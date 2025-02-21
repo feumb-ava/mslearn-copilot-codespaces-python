eeeea126de917d421bca8fbc21eac681652bfaf4
Fernanda Umberto <f.pereira.umberto@avanade.com>
2025-02-21 13:47:53 +0000

    commit inicial

--- webapp/main.py
+++ webapp/main.py
@@ -6,6 +6,7 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import hashlib
 
 current_dir = dirname(abspath(__file__))
 static_path = join(current_dir, "static")
@@ -26,12 +27,15 @@ def root():
 
 @app.post('/generate')
 def generate(body: Body):
-    """
-    Generate a pseudo-random token ID of twenty characters by default. Example POST request body:
-
-    {
-        "length": 20
-    }
-    """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+
+class Text(BaseModel):
+    text: str
+
+
+@app.post('/checksum')
+def checksum(body: Text):
+    checksum_value = hashlib.md5(body.text.encode('utf-8')).hexdigest()
+    return {'checksum': checksum_value}
